ebdae7902ce7657afd602c766fa7106f34f4fab9
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-04 19:12:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,8 @@
 def turn_count(board)
-  board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+  board =
   turn_count = 0
   board.each do |board|
         turn_count += 1
+  puts "#{turn_count}"
   end
 end
